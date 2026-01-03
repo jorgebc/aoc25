da3dfdcf88b634382e814c9db9b51b2c50b81543
Jorge B C <jorge.buffa.casalis@gmail.com>
2026-01-03 16:13:45 +0000

    Day 2 part two

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -4,15 +4,23 @@ fun main() {
     var productIdRanges = readProductIdRanges("day02/example-input.txt")
 
     var invalidIdSum: Long
-    invalidIdSum = day02PartOne(productIdRanges)
+    invalidIdSum = solve(productIdRanges, ::isInvalidPartOne)
     check(invalidIdSum == 1227775554L) { "Part 1 example fail, sum should be 1227775554 but was $invalidIdSum" }
 
     productIdRanges = readProductIdRanges("day02/input.txt")
-    invalidIdSum = day02PartOne(productIdRanges)
+    invalidIdSum = solve(productIdRanges, ::isInvalidPartOne)
     println("Part 1 invalid ids sum: $invalidIdSum")
+
+    productIdRanges = readProductIdRanges("day02/example-input.txt")
+    invalidIdSum = solve(productIdRanges, ::isInvalidPartTwo)
+    check(invalidIdSum == 4174379265L) { "Part 2 example fail, sum should be 4174379265 but was $invalidIdSum" }
+
+    productIdRanges = readProductIdRanges("day02/input.txt")
+    invalidIdSum = solve(productIdRanges, ::isInvalidPartTwo)
+    println("Part 2 invalid ids sum: $invalidIdSum")
 }
 
-fun day02PartOne(productIdRanges: List<Pair<Long, Long>>): Long {
+fun solve(productIdRanges: List<Pair<Long, Long>>, isInvalid: (Long) -> Boolean): Long {
 
     var invalidIdSum = 0L
 
@@ -30,14 +38,34 @@ fun day02PartOne(productIdRanges: List<Pair<Long, Long>>): Long {
     return invalidIdSum
 }
 
-fun isInvalid(productId: Long): Boolean {
+fun isInvalidPartOne(productId: Long): Boolean {
     val (firstHalf, secondHalf) = splitInHalf(productId.toString())
     return firstHalf == secondHalf
+}
+
+fun isInvalidPartTwo(productId: Long): Boolean {
+    val productIdString = productId.toString()
+    val maxParts = productIdString.length
+
+    for (numOfParts in 2..maxParts) {
+        val parts = splitIn(productIdString, numOfParts)
+        if (parts.toSet().size == 1) return true
+    }
+
+    return false
 }
 
 fun splitInHalf(input: String): Pair<String, String> {
     val mid = input.length / 2
     return input.substring(0, mid) to input.substring(mid)
+}
+
+fun splitIn(input: String, numOfParts: Int): List<String> {
+    if (input.length % numOfParts != 0) return emptyList()
+    else {
+        val size = input.length / numOfParts
+        return input.chunked(size)
+    }
 }
 
 fun readProductIdRanges(filePath: String): List<Pair<Long, Long>> =
--- src/test/kotlin/Day02Test.kt
+++ src/test/kotlin/Day02Test.kt
@@ -1,6 +1,8 @@
 package day02
 
-import day02PartOne
+import isInvalidPartOne
+import isInvalidPartTwo
+import solve
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -10,8 +12,17 @@ class Day02Test {
     fun `Part one should sum simple invalid ids, ids without 0`() {
 
         val productIdRanges = listOf(Pair(11L, 22L))
-        val result = day02PartOne(productIdRanges)
+        val result = solve(productIdRanges, ::isInvalidPartOne)
 
         assertEquals(33, result)
+    }
+
+    @Test
+    fun `Part two should sum if number repeats three times`() {
+
+        val productIdRanges = listOf(Pair(99L, 111L))
+        val result = solve(productIdRanges, ::isInvalidPartTwo)
+
+        assertEquals(210, result)
     }
 }
